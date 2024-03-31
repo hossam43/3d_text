66ec51d90084d7f339496e02a53c0f479ba8cf7c
hossam43 <hossamayman876.gmail.com>
2024-03-31 23:33:22 +0200

    add vercel

--- threeDText.js
+++ threeDText.js
@@ -3,8 +3,7 @@
 "use strict";
 // Importing
 import "./style.css";
-// import * as THREE from "three";
-import * as THREE from "../node_modules/three/build/three.module.js";
+import * as THREE from "three";
 import * as dat from "dat.gui";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
